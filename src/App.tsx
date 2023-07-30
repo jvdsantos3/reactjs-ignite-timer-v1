c66f7c54d2e67395d88d34afd572d30f3e897722
João <jv.dossantos012@gmail.com>
2023-07-29 23:08:09 -0300

    Projeto

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/themes/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
