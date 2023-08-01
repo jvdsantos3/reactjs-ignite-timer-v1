357e6feb4f2df77a8d13c8fba3fdbe8c5b50b405
João <jv.dossantos012@gmail.com>
2023-08-01 16:56:30 -0300

    fix: import of global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/themes/global'
+import { GlobalStyle } from './styles/global'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 import { CyclesContextProvider } from './contexts/CyclesContext'
